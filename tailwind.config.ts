c0537597003a946240f14675c55057499a2ff97e
PRAVEENKUMAR K <praveenkum1225@gmail.com>
2025-12-18 02:52:00 +0530

    tested

---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from "tailwindcss";
+import { heroui } from "@heroui/react";
+
+const config: Config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@heroui/react/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [heroui()],
+};
+
+export default config;
